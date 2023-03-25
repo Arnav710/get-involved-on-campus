22cab59987b558e8dc25e9bd826166f22016c5d7
Arnav Modi <arnav@Arnavs-MacBook-Pro-2.local>
2023-03-25 12:26:17 -0700

    rest api basic string matching search

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const data = require("./data.json");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post("/api/search", (req, res) => {
+  const { queries } = req.body;
+
+  const results = data.filter((org) =>
+    queries.some(
+      (query) =>
+        org.name.toLowerCase().includes(query.toLowerCase()) ||
+        org.description.toLowerCase().includes(query.toLowerCase())
+    )
+  );
+
+  res.json(results);
+});
+
+app.listen(4000, () => {
+  console.log("Server is listening on port 4000");
+});
