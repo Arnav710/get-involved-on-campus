8ea0c551c5ed42dffd0f0a9e652f57d23f35403f
Arnav Modi <arnav@Arnavs-MacBook-Pro-2.local>
2023-03-30 08:36:49 -0400

    modified uUpvote schema

--- backend/models/Upvote.js
+++ backend/models/Upvote.js
@@ -1,16 +1,20 @@
 const mongoose = require('mongoose');
 
-const Upvote = new mongooge.Schema({
+const Upvote = new mongoose.Schema({
+    organizationName: {
+        type: String,
+        required: true
+    },
     postId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Post',
-        required: true,
+        required: true
     },
-    userId: {
+    upvotedBy: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
-        required: true,
-    },
-})
+        required: true
+    }]
+});
 
 module.exports = mongoose.model('Upvote', Upvote);
